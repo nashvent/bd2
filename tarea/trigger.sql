612a2bf991d2daf4a2cda0f6b34f9edb0bc2563b
nashvent <anibal.ventura@ucsp.edu.pe>
2019-04-10 18:59:14 -0500

    Mejoras en trigger

--- tarea/trigger.sql
+++ tarea/trigger.sql
@@ -44,7 +44,9 @@ CREATE TABLE auditoria (
 DROP TRIGGER antes_ins_alumnos;
 SHOW triggers;
 
-/******* TRIGGER ********/
+
+
+/******* TRIGGER ALUMNOS ********/
 DELIMITER //
 Create Trigger ins_alumnos BEFORE INSERT ON alumnos FOR EACH ROW
 BEGIN
@@ -54,11 +56,62 @@ END //
 DELIMITER //
 Create Trigger upd_alumnos BEFORE UPDATE ON alumnos FOR EACH ROW
 BEGIN
-INSERT INTO auditoria (tabla,columna,val_ant,val_nuevo) VALUES ('alumnos','nombre',NEW.nombre);
+INSERT INTO auditoria (tabla,columna,val_ant,val_nuevo) VALUES ('alumnos','nombre',OLD.nombre,NEW.nombre);
+END //
+
+DELIMITER //
+Create Trigger del_alumnos BEFORE DELETE ON alumnos FOR EACH ROW
+BEGIN
+INSERT INTO auditoria (tabla,columna,val_ant) VALUES ('alumnos','nombre',OLD.nombre);
 END //
 
 
-INSERT INTO alumnos (nombre) VALUES ('nash');
+/******* TRIGGER CURSOS ********/
+DELIMITER //
+Create Trigger ins_curso BEFORE INSERT ON curso FOR EACH ROW
+BEGIN
+INSERT INTO auditoria (tabla,columna,val_nuevo) VALUES ('curso','nombre',NEW.nombre);
+END //
+
+DELIMITER //
+Create Trigger upd_curso BEFORE UPDATE ON curso FOR EACH ROW
+BEGIN
+INSERT INTO auditoria (tabla,columna,val_ant,val_nuevo) VALUES ('curso','nombre',OLD.nombre,NEW.nombre);
+END //
+
+DELIMITER //
+Create Trigger del_curso BEFORE DELETE ON curso FOR EACH ROW
+BEGIN
+INSERT INTO auditoria (tabla,columna,val_ant) VALUES ('curso','nombre',OLD.nombre);
+END //
+
+
+/******* TRIGGER MATRICULLA ********/
+DELIMITER //
+Create Trigger ins_matricula BEFORE INSERT ON matricula FOR EACH ROW
+BEGIN
+INSERT INTO auditoria (tabla,columna,val_nuevo) VALUES ('matricula','id_alu',NEW.id_alu);
+INSERT INTO auditoria (tabla,columna,val_nuevo) VALUES ('matricula','id_curso',NEW.id_curso);
+END //
+
+
+DELIMITER //
+Create Trigger upd_matricula BEFORE UPDATE ON matricula FOR EACH ROW
+BEGIN
+INSERT INTO auditoria (tabla,columna,val_ant,val_nuevo) VALUES ('matricula','id_alu',OLD.id_alu,NEW.id_alu);
+INSERT INTO auditoria (tabla,columna,val_ant,val_nuevo) VALUES ('matricula','id_curso',OLD.id_curso,NEW.id_curso);
+END //
+
+DELIMITER //
+Create Trigger del_matricula BEFORE DELETE ON matricula FOR EACH ROW
+BEGIN
+INSERT INTO auditoria (tabla,columna,val_ant) VALUES ('matricula','id_alu',OLD.id_alu);
+INSERT INTO auditoria (tabla,columna,val_ant) VALUES ('matricula','id_curso',OLD.id_curso);
+END //
+
+
+INSERT INTO alumnos (nombre) VALUES ('nacho');
+INSERT INTO matricula (id_alu,id_curso) VALUES (3,10);
 
 /*
 DELIMITER //
